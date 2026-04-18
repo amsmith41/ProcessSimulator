c16f6be9a2572759b64118ffac665ee1c26d11d8
Adrian <adrian.m.smith@wsu.edu>
2026-04-17 23:51:22 -0700

    Added burst function prototypes in Process.h

--- include/Process.h
+++ include/Process.h
@@ -17,12 +17,25 @@ enum class ProcessState
     Terminated
 };
 
+struct BurstStep
+{
+    // CPU/IO bursts are fundamental in driving processs state changes.
+    // Without these, states are just labels with no behavior
+    bool isCPU; // True if the type of burst is a CPU burst, false if an I/O burst
+    int duration; // Duration of the burst
+};
+
+
 class Process
 {
     private:
         int pid; // Process ID
         ProcessState state; // Current state of the process
-        int arrivalTime; // Time when the process arrived in the system        
+        int arrivalTime; // Time when the process arrived in the system
+
+        std::vector<BurstStep> burstTimes; // List of CPU burst times for the process
+        int currentBurstIndex; // Index of the current CPU burst being executed
+        int remainingBurstTime; // Remaining time for the current CPU burst
 
         int totalRemainingCPUTime; // Total remaining CPU time for the process (useful for SJN and STCF)
 
@@ -40,6 +53,12 @@ class Process
         int getArrivalTime() const; // Get the arrival time of the process
         ProcessState getState() const; // Get the current state of the process
         void setState(ProcessState newState); // Set the state of the process
+
+        int getCurrentBurstTime() const; // Get the current CPU burst time
+        int getRemainingBurstTime() const; // Get the remaining time for the current CPU burst
+        bool hasMoreBursts() const; // Check if the process has more CPU bursts to execute
+        bool isCurrentBurstCPU() const; // Check if the current burst is a CPU burst
+        bool isCurrentBurstIO() const; // Check if the current burst is an I/O burst
 
         int getTotalRemainingCPUTime() const; // Get the total remaining CPU time for the process
         int getWaitingTime() const; // Get the total waiting time for the process
