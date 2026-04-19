207d247d84180fb9c3deb3115d40b730dac291fe
Adrian <adrian.m.smith@wsu.edu>
2026-04-18 20:25:37 -0700

    Added comments todo in Simulation.cpp

---
+++ src/Simulation.cpp
@@ -0,0 +1,18 @@
+#include "../include/Simulation.h"
+#include "../include/Process.h"
+#include "../include/Ischeduler.h"
+
+while(true)
+{
+    // Get arrivals
+    // If no running process, get one from the scheduler
+    // Execute one tick of the running process, update scheduler with tick
+    // Handle completion of process
+    // Advance the time
+    ///////////////
+    // Later integration will involve
+    // Time slicing for RRobin
+    // Full i/o blocking and unblocking logic
+    // logging and output of results
+    break; // Placeholder
+}
