898cd7fc1547a1d7cccb1fb78d652240e328efd0
Adrian <adrian.m.smith@wsu.edu>
2026-04-18 21:23:01 -0700

    If there exists no running process, get one from the scheduler

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -14,13 +14,28 @@ void Simulation::run()
     while (!allProcessesCompleted) {
         
             // Get the newly arrived process and add it to the ready queue
-            for (Process* process : processes) {
+            for (Process* process : processes)
+            {
                 if (process->getState() == ProcessState::New && process->getArrivalTime() <= currentTime)
                 {
                     process->setState(ProcessState::Ready); // New -> Ready state
                     scheduler->addProcess(process);
                 }
             }
+
+            // When the runningProcess == nullptr, the CPU is idle 
+            // So we need to get the next process to run from the scheduler
+            if (runningProcess == nullptr && scheduler->hasReadyProcesses())
+            {
+                runningProcess = scheduler->getNextProcess(currentTime);
+                if (runningProcess != nullptr) 
+                {
+                    runningProcess->setState(ProcessState::Running);
+                }
+            }
+
+            
+
         
     }
 }
