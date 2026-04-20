4ad8655224bb785e15620ab5adaccd64a7e6fb9a
Adrian <adrian.m.smith@wsu.edu>
2026-04-20 01:06:05 -0700

    Defined mapping a virtual page to a physical frame

--- src/PageTable.cpp
+++ src/PageTable.cpp
@@ -1,7 +1,17 @@
 #include "../include/PageTable.h"
 
+// Initialization of all entries as unmapped
 PageTable::PageTable(int numPages)
 {
     entries.resize(numPages, {-1, false}); // Initialize all entries as unmapped
 }
 
+// Map virtual page to a physical frame
+void PageTable::mapPage(int virtualPageNumber, int physicalFrameNumber)
+{
+    if (virtualPageNumber >= 0 && virtualPageNumber < entries.size())
+    {
+        entries[virtualPageNumber].valid = true;
+        entries[virtualPageNumber].physicalFrameNumber = physicalFrameNumber;
+    }
+};
