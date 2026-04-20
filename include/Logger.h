43f9e5e752ebfa437c9edeb4eae841c178b92256
Adrian <adrian.m.smith@wsu.edu>
2026-04-19 20:57:02 -0700

    Added logger function prototypes with intent to log to a file

--- include/Logger.h
+++ include/Logger.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include <fstream>
 
 // Types of events that will occur during the simulation
 enum class EventType
@@ -10,4 +13,26 @@ enum class EventType
     Block, // Process is blocked for I/O
     Unblock, // Process finishes I/O and is unblocked
     Terminate // Process finished all its bursts and terminates
+};
+
+struct LogEntry
+{
+    int time; // Simulation time when event occurs
+    int processId; // ID of the process involved in the event
+    EventType eventType; // Type of event that occurred
+};
+
+class Logger
+{
+    private:
+        std::vector<LogEntry> logEntries; // Object to hold all log entries
+        std::ofstream outFile; // Output file stream when logging to a file
+
+    public:
+        Logger(const std::string& filename); // Initialize logger and open file stream
+        ~Logger(); // Close file stream if open
+        
+        void logEvent(int time, int processId, EventType eventType); // Log an event with time, ProcessID, and the type of event
+        const std::vector<LogEntry>& getLogEntries() const; // Get log entires
+        void print() const; // Print log entries
 };
